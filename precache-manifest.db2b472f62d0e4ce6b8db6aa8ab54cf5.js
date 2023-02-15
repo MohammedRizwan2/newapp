68093204a42f0c8465bfb94be6ff5698985ca47e
MohammedRizwan2 <114277296+MohammedRizwan2@users.noreply.github.com>
2023-02-15 15:51:15 +0530

    Updates

---
+++ precache-manifest.db2b472f62d0e4ce6b8db6aa8ab54cf5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "399bfef5a565d15eae28",
+    "url": "/MohammedRizwan2/newapp/static/css/main.0b5066a6.chunk.css"
+  },
+  {
+    "revision": "399bfef5a565d15eae28",
+    "url": "/MohammedRizwan2/newapp/static/js/main.29df4d11.chunk.js"
+  },
+  {
+    "revision": "7edce0e3e9fd084d6def",
+    "url": "/MohammedRizwan2/newapp/static/js/runtime~main.3c219ff7.js"
+  },
+  {
+    "revision": "0416358c565c038e7b7c",
+    "url": "/MohammedRizwan2/newapp/static/js/2.3d94c40f.chunk.js"
+  },
+  {
+    "revision": "67ab349aa5ef1eae88f0",
+    "url": "/MohammedRizwan2/newapp/static/js/3.2999b699.chunk.js"
+  },
+  {
+    "revision": "42d870bf725896a08a87593b401d35b1",
+    "url": "/MohammedRizwan2/newapp/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/MohammedRizwan2/newapp/precache-manifest.db2b472f62d0e4ce6b8db6aa8ab54cf5.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/MohammedRizwan2/newapp/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
